3952dc7865eba0e4809a01581624db34bb627275
Abdurahman <abdorehman@gmail.com>
2023-08-09 19:09:25 -0600

    Add /urls.json route that will return the urlDatabase object as a JSON response

--- express_server.js
+++ express_server.js
@@ -1,16 +1,22 @@
-const express = require("express");//Require the express library
-const app = express();// Define our app as an instance of express
+const express = require("express"); //Require the express library
+const app = express(); // Call our app as an instance of express
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
+// Route that responds with "Hello!" for requests to the root path(/)
+app.get("/", (req, res) => {  
   res.send("Hello!");
 });
 
+// Route that will return the urlDatabase object as a JSON response
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//Make the server listen on port 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
