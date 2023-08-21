b7be2b168af5500b9b3113324e0804b9ca6d7803
Abdurahman <abdorehman@gmail.com>
2023-08-20 22:28:41 -0600

    Implement more permission features

--- express_server.js
+++ express_server.js
@@ -53,10 +53,27 @@ const getUserByEmail = (userEmail) => {
   return null;
 };
 
+// Return URLS for currently logged in user
+const urlsForUser = (id, urlDatabase) => {
+  const userURLs = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
+};
+
 //Database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -99,7 +116,7 @@ app.get("/urls", (req, res) => {
   const userObj = users[req.cookies.user_id];
   const templateVars = {
     user: userObj,
-    urls: urlDatabase
+    urls: urlsForUser(req.cookies.user_id, urlDatabase)
   };
   res.render("urls_index", templateVars);
 });
@@ -116,9 +133,14 @@ app.post("/urls", (req, res) => {
     return;
   }
   const randomString = generateRandomString(); //Generate a unique id for shortURL
+  const longURL = req.body.longURL;
+  const userId = req.cookies.user_id;
   // save the id and longURL pair to urlDatabase 
-  urlDatabase[randomString] = req.body.longURL;
-  // console.log("New urlDB:", urlDatabase);
+  urlDatabase[randomString] = {
+    longURL: longURL,
+    userID: userId
+  };
+  console.log("New urlDB:", urlDatabase);
   res.redirect(`/urls/${randomString}`);
 });
 
@@ -139,7 +161,7 @@ app.get("/urls/new", (req, res) => {
 // Route to redirect directly to longURL 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];//look up the longURL in urlDatabase
-  if (longURL === undefined) {
+  if (!longURL) {
     res.status(404).send("Short URL does not exist");
   } else {
     res.redirect(302, longURL);
@@ -152,6 +174,22 @@ app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL]; // Look up the corresponding longURL from the urlDatabase using the extracted shortURL
   const userObj = users[req.cookies.user_id];
+
+  // Check to see if a user is logged in or not
+  if (!userObj) {
+    return res.status(403).send("Please log in first");
+  }
+
+  // Check if the URL does not exist
+  if (!longURL) {
+    return res.status(404).send("Short URL does not exist");
+  }
+
+  // Check if the user does not own the URL
+  if (longURL.userID !== userObj.id) {
+    return res.status(403).send("You don’t have appropriate permission to view this URL. Please contact your administrator!");
+  }
+
   /*Create an object containing the extracted shortURL and the corresponding longURL*/
   const templateVars = { id: shortURL, longURL: longURL, user: userObj };
   res.render("urls_show", templateVars);
@@ -160,15 +198,48 @@ app.get("/urls/:id", (req, res) => {
 // Delete route
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect("/urls");
+  // const userID = req.cookies.userID;
+  const userObj = users[req.cookies.user_id];
+  const userID = userObj && userObj.id;
+  const urlInfo = urlDatabase[id];
+
+  //check if the ID does not exist
+  if (!urlInfo) {
+    return res.status(401).send("Short URL not found!");
+  } else if (!userID) { //check if user is not logged in
+    return res.status(403).send("You must log in or register first!");
+
+  } else if (urlDatabase[id].userID !== userID) { //check if user doesnot own the url
+    return res.status(403).send("You don’t have appropriate permission to delete URL. Please contact your administrator!");
+  } else {
+    delete urlDatabase[id];
+    res.redirect("/urls");
+  }
 });
 
 //Update route
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
+  const userObj = users[req.cookies.user_id];
+  const userID = userObj && userObj.id;
+
+  //check if the URL ID does not exist
+  if (!urlDatabase[id]) {
+    return res.status(404).send("Short URL does not exist");
+  }
+  //check if user is not logged in
+  if (!userObj) {
+    return res.status(403).send("Please log in/register first");
+  }
+
+  //check if the user doesnot own the url
+  if (urlDatabase[id].userID !== userID) {
+    return res.status(403).send("You don’t have appropriate permission to Edit URL. Please contact your administrator!");
+  }
+
+  //update URL if user owns the URL
   const newURL = req.body.longURL;
-  urlDatabase[id] = newURL;
+  urlDatabase[id].longURL = newURL;
   res.redirect("/urls");
 });
 
@@ -239,11 +310,11 @@ app.post("/register", (req, res) => {
   //Get the data from the html body
   const userEmail = req.body.email;
   const userPassword = req.body.password;
-  //Check if email or password are empty
+  // Check if username and password is provided
   if (!userEmail || !userPassword) {
-    res.status(400).end("<p>Please Enter Email and/or Password!</p>");
+    res.status(400).send("Please Enter Email and/or Password!</p>");
   } else if (getUserByEmail(userEmail)) { //check if email already exist or not
-    res.status(400).send("<p>Email address is already exists !!!</p>");
+    res.status(400).send("Email address is already exists !!!");
   } else {
     // Create new user object
     const newUser = {
